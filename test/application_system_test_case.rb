87d621d10f7681290e29823fde2689cf12a8c252
thiagoc7 <thiagoc7@gmail.com>
2025-08-04 18:06:56 +0000

    fix: resolve CI issues
    - Fix RuboCop layout error: remove spaces inside array brackets
    - Fix Docker error: remove invalid --disable-dev-shm-usage flag
    - Both lint and test jobs should now pass

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,6 +7,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
       capabilities: [:chrome]
     }
   else
-    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
   end
 end
