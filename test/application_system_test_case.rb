b61f423cd61b64f1c65e5dd8a664695f0cb01e59
thiagoc7 <thiagoc7@gmail.com>
2025-08-04 18:03:36 +0000

    feat: use Selenium standalone container for CI
    - Replace manual Chrome/ChromeDriver installation with selenium/standalone-chrome container
    - Much simpler and more reliable approach
    - No more version compatibility issues
    - Container comes with Chrome and ChromeDriver pre-configured
    - Update system test configuration to use remote Selenium when available

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  if ENV["SELENIUM_REMOTE_URL"]
+    driven_by :selenium, using: :chrome, options: {
+      url: ENV["SELENIUM_REMOTE_URL"],
+      capabilities: [:chrome]
+    }
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
 end
