721f4390da5dd03e632fc43896d0c7cbefc353f7
thiagoc7 <thiagoc7@gmail.com>
2025-08-05 01:25:29 +0000

    feat: configure Selenium system tests and remove deploy.yml

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,10 +2,16 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV["SELENIUM_REMOTE_URL"]
-    driven_by :selenium, using: :chrome, options: {
-      url: ENV["SELENIUM_REMOTE_URL"],
-      capabilities: [ :chrome ]
-    }
+    chrome_options = Selenium::WebDriver::Chrome::Options.new
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    driven_by :selenium,
+              using: :chrome,
+              options: chrome_options,
+              url: ENV["SELENIUM_REMOTE_URL"]
   else
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
