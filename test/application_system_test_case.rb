aa685a09ecc7aaf25914ee11f70924493f9b4063
thiagoc7 <thiagoc7@gmail.com>
2025-08-05 01:21:12 +0000

    fix: restore working system tests configuration

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,9 +4,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   if ENV["SELENIUM_REMOTE_URL"]
     driven_by :selenium, using: :chrome, options: {
       url: ENV["SELENIUM_REMOTE_URL"],
-      capabilities: [:chrome]
+      capabilities: [ :chrome ]
     }
   else
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
   end
 end
